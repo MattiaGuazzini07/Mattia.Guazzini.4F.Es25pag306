ac0340bfdf146951782f1d9e2d2f982ca1115fb0
Rohozin Maksym <rohozin.max@gmail.com>
2025-02-21 10:19:29 +0100

    Add MedicinalName property and update constructor in Medicinal class

--- Mattia.Guazzini.4F.Es25pag306/Medicinal.cs
+++ Mattia.Guazzini.4F.Es25pag306/Medicinal.cs
@@ -4,11 +4,15 @@ public class Medicinal : Pharmacy
 {
     public string MedicinalName { get; set; }
     public double MedicinalPrice { get; set; }
-    public Medicinal(string name, int nMedicinals, double medicinalPrice) : base(name, nMedicinals)
+    public Medicinal(string name, int nMedicinals, double medicinalPrice, string medicinalName) : base(name, nMedicinals)
     {
         MedicinalPrice = medicinalPrice;
+        MedicinalName = medicinalName;
     }
-    
+    /// <summary>
+    /// Description of the object
+    /// </summary>
+    /// <returns>Description string</returns>
     public override string ToString()
     {
         return $"Name: {Name}, Medicinals number: {NMedicinals}, Price: {MedicinalPrice}";
